baaee50c7516fcbc2f0a46a41916ad267f421c66
adnan-kibria <adnan.official.rb000@gmail.com>
2025-07-19 21:12:35 +0600

    Testing

---
+++ Test/demotest.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>This is web tech section F</h1>
+        <h2>We are doing web tech class now</h2>
+        <h2 style="background-color: aqua;">
+            Lets use background color
+        </h2>
+        <p><i><b>This is with para tag</b></i></p>
+        <a href="https://www.aiub.edu">AIUB</a>
+        <p style="color: blue;">
+            This is a paragraph with blue text.
+        </p>
+        <p style="color: rgb(0,255,255);"> RGB Color</p>
+        <img src="Bird.jpg" width="100" height="100" alt="Bird Image">
+    </body>
+</html>
