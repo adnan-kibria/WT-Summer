cde62964b09bb267affb6cc2c6648aa4628c330f
adnan-kibria <adnan.official.rb000@gmail.com>
2025-08-03 16:51:37 +0600

    chnages

--- Midterm/L3/View/task_3_valid.js
+++ Midterm/L3/View/task_3_valid.js
@@ -9,11 +9,29 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function validateForm() {
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const phone = document.getElementById("phone").value;
     const email = document.getElementById("email").value;
     const createpass = document.getElementById("createpass").value;
     const confirmpass = document.getElementById("confirmpass").value;
     const otherAmount = document.getElementById("other_amount").value;
 
+    if(!validateFname(fname))
+    {
+        alert("Invalid input")
+        return false;
+    }
+    if(!validateLname(lname))
+    {
+        alert("Invalid input")
+        return false;
+    }
+    if(!validatePhone(phone))
+    {
+        alert("Invalid input")
+        return false;
+    }
     if (!validateEmail(email)) {
         alert("Invalid email format");
         return false;
@@ -32,4 +50,19 @@ function validateForm() {
 function validateEmail(email) {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email);
+}
+function validateFname(fname)
+{
+    const re = /^[a-zA-Z]+$/;
+    return re.test(fname);
+}
+function validateLname(lname)
+{
+    const re = /^[a-zA-Z]+$/;
+    return re.test(lname);
+}
+function validatePhone(phone)
+{
+    const re = /^\d{11}$/;
+    return re.test(phone);
 }
